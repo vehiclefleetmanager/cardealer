cf2cf37a549e312abbbd877a9ccfb717d0bccb27
vehiclefleetmanager <tmm.miernik@gmail.com>
2018-09-23 10:23:32 +0200

    first configure Spirng Application

---
+++ src/main/java/com/example/cardealer/CardealerApplication.java
@@ -0,0 +1,12 @@
+package com.example.cardealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardealerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardealerApplication.class, args);
+    }
+}
